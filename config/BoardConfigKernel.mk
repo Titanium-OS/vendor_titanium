BlissRoms/platform_vendor_bliss

kernel: Fix ccache not being used when building
When updating commits from Pie->Q the ccache varible
changed and I left an instance of the old one.

Change-Id: I16f2e5c8d135be1471c974993f778aec7e9d5d68
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

config/BoardConfigKernel.mk
@@ -82,9 +82,13 @@ endif
 KERNEL_TOOLCHAIN_PATH_gcc := $(KERNEL_TOOLCHAIN_$(KERNEL_ARCH))/$(KERNEL_TOOLCHAIN_PREFIX_$(KERNEL_ARCH))
 
 ifneq ($(USE_CCACHE),)
-    ifneq ($(CCACHE_EXEC),)
-        # Android 10+ deprecates use of a build ccache. Only system installed ones are now allowed
-        CCACHE_BIN := $(CCACHE_EXEC)
+    ifeq ($(USE_SYSTEM_CCACHE),)
+        CCACHE_BIN := $(BUILD_TOP)/prebuilts/tools-extras/$(HOST_PREBUILT_TAG)/bin/ccache
+        # Check that the executable is here.
+        CCACHE_BIN := $(strip $(wildcard $(ccache)))
+    else
+        # Detect if the system already has ccache installed to use instead of the prebuilt
+        CCACHE_BIN := $(shell which ccache)
     endif
 endif
 

Titanium-OS/vendor_titanium

config/BoardConfigKernel.mk
@@ -82,9 +82,13 @@ endif
 KERNEL_TOOLCHAIN_PATH_gcc := $(KERNEL_TOOLCHAIN_$(KERNEL_ARCH))/$(KERNEL_TOOLCHAIN_PREFIX_$(KERNEL_ARCH))
 
 ifneq ($(USE_CCACHE),)
-    ifneq ($(CCACHE_EXEC),)
-        # Android 10+ deprecates use of a build ccache. Only system installed ones are now allowed
-        CCACHE_BIN := $(CCACHE_EXEC)
+    ifeq ($(USE_SYSTEM_CCACHE),)
+        CCACHE_BIN := $(BUILD_TOP)/prebuilts/tools-titanium/$(HOST_PREBUILT_TAG)/bin/ccache
+        # Check that the executable is here.
+        CCACHE_BIN := $(strip $(wildcard $(ccache)))
+    else
+        # Detect if the system already has ccache installed to use instead of the prebuilt
+        CCACHE_BIN := $(shell which ccache)
     endif
 endif
 