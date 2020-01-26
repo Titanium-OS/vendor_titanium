shakhawat3742/vendor_aosp

vendor: Use python script to fetch official devices
Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(curl -s https://raw.githubusercontent.com/PixelExperience/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
+for device in $(python vendor/aosp/tools/get_official_devices.py)
 do
 for var in eng user userdebug; do
 add_lunch_combo aosp_$device-$var

Titanium-OS/vendor_titanium

vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(curl -s https://raw.githubusercontent.com/Titanium-OS-Devices/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
+for device in $(python vendor/titanium/tools/get_official_devices.py)
 do
 for var in eng user userdebug; do
 add_lunch_combo titanium_$device-$var