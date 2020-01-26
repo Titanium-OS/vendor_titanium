AospExtended/platform_vendor_aosp

vendorsetup: Enable also other build variants

vendorsetup.sh
@@ -1,4 +1,6 @@
 for device in $(curl -s https://raw.githubusercontent.com/AospExtended/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
 do
-add_lunch_combo aosp_$device-userdebug
+for var in eng user userdebug; do
+add_lunch_combo aosp_$device-$var
+done
 done

Titanium-OS/vendor_titanium

vendorsetup.sh
@@ -1,4 +1,6 @@
 for device in $(curl -s https://raw.githubusercontent.com/Titanium-OS-Devices/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
 do
-add_lunch_combo titanium_$device-userdebug
+for var in eng user userdebug; do
+add_lunch_combo titanium_$device-$var
+done
 done
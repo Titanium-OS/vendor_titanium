422f1d34a0f797c5c121154d87e0d01f7f6d3fb6
Joe <joe2k01dev@gmail.com>
2017-07-20 19:27:01 +0200

    vendorsetup: Enable also other build variants

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 for device in $(curl -s https://raw.githubusercontent.com/Titanium-OS-Devices/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
 do
-add_lunch_combo titanium_$device-userdebug
+for var in eng user userdebug; do
+add_lunch_combo titanium_$device-$var
+done
 done
