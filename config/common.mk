3b40cc651bb6c5a1333a380a64d972321e4a939b
Saikat Halder <iamdevsaikat@gmail.com>
2020-02-10 22:18:47 +0530

    vendor: nuke lineage default Charger
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -144,10 +144,6 @@ PRODUCT_COPY_FILES += \
     vendor/titanium/prebuilt/fonts/Surfer/Surfer.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Surfer.ttf \
     vendor/titanium/prebuilt/fonts/HYCoffee/HYCoffee.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/HYCoffee.ttf
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
