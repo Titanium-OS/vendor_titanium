Havoc-OS/android_vendor_havoc

vendor: Add Google privapp permissions
Added from https://github.com/omnirom/android_vendor_omni/blob/android-10/prebuilt/etc/permissions/privapp-permissions-elgoog.xml. Credits to @darkobas, @golden-guy, @Vachounet and @micky387

config/common.mk
@@ -85,8 +85,9 @@ PRODUCT_COPY_FILES += \
 
 # This is Havoc!
 PRODUCT_COPY_FILES += \
-    vendor/havoc/config/permissions/privapp-permissions-havoc-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-havoc.xml \
-    vendor/havoc/config/permissions/privapp-permissions-havoc-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-havoc.xml
+    vendor/havoc/config/permissions/privapp-permissions-google_prebuilt.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google_prebuilt.xml \
+    vendor/havoc/config/permissions/privapp-permissions-havoc-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-havoc.xml \
+    vendor/havoc/config/permissions/privapp-permissions-havoc-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-havoc.xml
 
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \

Titanium-OS/vendor_titanium

Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

config/common.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
 
 # This is titanium!
 PRODUCT_COPY_FILES += \
+    vendor/titanium/config/permissions/privapp-permissions-google_prebuilt.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google_prebuilt.xml \
     vendor/titanium/config/permissions/privapp-permissions-titanium.xml:system/etc/permissions/privapp-permissions-titanium.xml
 
 # Power whitelist