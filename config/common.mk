bd821889ab75ed0339411bb065b19d2fe3e695e4
Michael Bestas <mkbestas@lineageos.org>
2020-03-10 23:32:39 +0200

    config: Build Seedvault
    Change-Id: If858ce8bc3167b95cb5206be77f23cb8632f763b

--- config/common.mk
+++ config/common.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Seedvault
+PRODUCT_PACKAGES += \
+    Seedvault
+
 # Extra tools in Titanium
 PRODUCT_PACKAGES += \
     7z \
