ac9a5cbdf2de911573a7dc0e9d80f5cffc3c0ae7
Bruno Martins <bgcngm@gmail.com>
2020-12-01 21:22:46 +0000

    titanium: Add Android Go specific configs
    Change-Id: I29f203b3fa19bc955976ee6c3da4142a4ab325bf

---
+++ config/common_full_go_phone.mk
@@ -0,0 +1,5 @@
+# Set Lineage specific identifier for Android Go enabled products
+PRODUCT_TYPE := go
+
+# Inherit full common Titanium stuff
+$(call inherit-product, vendor/titanium/config/common_full_phone.mk)
